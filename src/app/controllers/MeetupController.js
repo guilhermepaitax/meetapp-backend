4e9445ef3ced434292d250452922196c5845f177
Guilherme Paitax <guilhermepaitax16@gmail.com>
2019-09-05 17:09:51 -0300

    Cadastrando Meetups

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      banner_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const meetup = await Meetup.create({ ...req.body, user_id: req.userId });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();

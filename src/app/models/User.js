2623b54e7c6b7bc73366b8377b07e686a7224625
Guilherme Paitax <guilhermepaitax16@gmail.com>
2019-09-06 15:05:09 -0300

    Migration e Model das Inscrições

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,6 +26,8 @@ class User extends Model {
 
   static associate(models) {
     this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+    this.hasMany(models.Subscription);
+    this.hasMany(models.Meetup);
   }
 
   checkPassword(password) {
